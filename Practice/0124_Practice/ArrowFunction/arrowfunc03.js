cfe199dea034a0d6ebfbac1c71976300d1988ec0
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-01-25 13:31:11 +0200

    DONE

--- Practice/0124_Practice/ArrowFunction/arrowfunc03.js
+++ Practice/0124_Practice/ArrowFunction/arrowfunc03.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
 You are given a function triple that returns the result of its parameter multiplied by 3.
@@ -8,9 +8,8 @@ Rewrite it as an arrow function.
 /**
  * @param {number} value
  */
-function triple() {
 
-}
+triple = (n) => n * 3;
 
 // Sample usage - do not change
 console.log(triple(2)); // 6
--- Practice/0124_Practice/ArrowFunction/arrowfunc04.js
+++ Practice/0124_Practice/ArrowFunction/arrowfunc04.js
@@ -1,14 +1,11 @@
-'use strict';
+"use strict";
 
 /**
 In this challenge, you're asked to write the triple function from scratch as an arrow function.
 The function triple receives a parameter and returns the result of multiplying it by 3.
 **/
 
-
-function triple () {
-
-}
+triple = (n) => n * 3;
 
 // (optional) Sample usage
 console.log(triple(2));
