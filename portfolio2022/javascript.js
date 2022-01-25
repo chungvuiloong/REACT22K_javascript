fb6596754f458d1c0552e992e46bebecbe8c0bfb
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-01-25 22:07:10 +0200

    moved

--- portfolio2022/javascript.js
+++
@@ -1,8 +0,0 @@
-function meowUs() {
-  // 1st Prototype - Works!!! BUt need to get ID from HTML
-  // var meow = document.getElementById("meow");
-
-  return (
-    meow.play() + alert("Thank you for meowing us! We will meow back ASAP!")
-  );
-}
