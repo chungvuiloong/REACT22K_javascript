e3f2b2e9173edaa28d5a79ff082524c24172d288
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-01 21:57:19 +0200

    update

--- 03_week/BMI/javascript.js
+++ 03_week/BMI/javascript.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // const calculateBMI = () => {
 //   let height = document.querySelector("#height").value;
 //   let heightSquare = height * height;
@@ -22,30 +20,21 @@
 //   document.querySelector("#answer").textContent = text;
 // };
 
-function calculateBMI() {
+calculateBMI = () => {
   let height = document.querySelector("#height").value;
   let weight = document.querySelector("#weight").value;
   let result = ([weight / (height * height)] * 10000).toFixed(1); // works
   let diagnosis;
 
   if (result > 30) {
-    document.querySelector(
-      "#answer"
-    ).textContent = `My BMI is ${result}. You are obese! You need to layoff the burgers`;
-  } else if (29.9 < result && result > 25) {
-    document.querySelector(
-      "#answer"
-    ).textContent = `My BMI is ${result}. You are pre-obese! You need to eat less`;
-  } else if (24.9 < result && result > 18.5) {
-    document.querySelector(
-      "#answer"
-    ).textContent = `My BMI is ${result}. You are boring! So normal....`;
+    diagnosis = `Your BMI is ${result}. You are obese! You need to layoff the burgers`;
+  } else if (29.9 < result || result > 25) {
+    diagnosis = `Your BMI is ${result}. You are pre-obese! You need to eat less`;
+  } else if (24.9 < result || result > 18.5) {
+    diagnosis = `Your BMI is ${result}. You are boring! So normal....`;
   } else if (result < 18.5) {
-    document.querySelector(
-      "#answer"
-    ).textContent = `My BMI is ${result}. You need to eat more!`;
+    diagnosis = `Your BMI is ${result}. You need to eat more!`;
   } else {
-    document.querySelector("#answer").textContent = `Something is wrong`;
+    diagnosis = `Something is wrong`;
   }
-  console.log(result, diagnosis);
-}
+};
