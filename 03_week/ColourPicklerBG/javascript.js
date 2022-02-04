02fc15613b8979e60f5915054cdbe890d82b6d2b
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-04 09:27:13 +0200

    updated

--- 03_week/ColourPicklerBG/javascript.js
+++ 03_week/ColourPicklerBG/javascript.js
@@ -4,29 +4,29 @@ let section = document.querySelector("button");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 
-// let direction = document.querySelectorAll('input[name="direction"]');
+let direction = document.querySelectorAll('input[name="direction"]');
 
-let direction = document.querySelectorAll(".direction1.direction2");
+// let direction = document.querySelectorAll(".direction1.direction2");
 
 // Margits solution
-// const setGradient = () => {
-//   console.log(color1.value);
-//   console.log(color2.value);
+const setGradient = () => {
+  console.log(color1.value);
+  console.log(color2.value);
 
-//   let selectedValue;
+  let selectedValue;
 
-//   for (const sel of direction) {
-//     if (sel.checked) {
-//       selectedValue = sel.value;
-//     }
-//   }
+  for (const sel of direction) {
+    if (sel.checked) {
+      selectedValue = sel.value;
+    }
+  }
 
-//   console.log(selectedValue);
+  console.log(selectedValue);
 
-//   document.body.style.backgroundImage = `linear-gradient(${selectedValue},${color1.value},${color2.value})`;
-// };
+  document.body.style.backgroundImage = `linear-gradient(${selectedValue},${color1.value},${color2.value})`;
+};
 
-// form.addEventListener("change", setGradient);
+form.addEventListener("change", setGradient);
 
 // Click Counter
 let clicks = 0;
