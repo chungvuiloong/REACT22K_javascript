9b1ebd1136efec084eda86d42c8a2a7b2cbdc6c7
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-08 13:31:38 +0200

    dark mode add

--- Practice/0207_Practice/DOM_inter/dom25/index.js
+++ Practice/0207_Practice/DOM_inter/dom25/index.js
@@ -1,1 +1,11 @@
 // TODO: clicking on 'Toggle dark mode' should add/remove 'dark' class on <html>
+
+let darkButton = document.querySelector("#theme-btn");
+
+darkButton.addEventListener("click", () => {
+  document.querySelector("html").classList.add("dark");
+});
+
+// darkButton.addEventListener("click", () => {
+//   document.querySelector("html").classList.remove("dark");
+// });
