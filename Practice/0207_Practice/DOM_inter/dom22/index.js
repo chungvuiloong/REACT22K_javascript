5ccc910c48836188a210946ddda0593c9ff159fa
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-08 13:21:20 +0200

    did upto 24

--- Practice/0207_Practice/DOM_inter/dom22/index.js
+++ Practice/0207_Practice/DOM_inter/dom22/index.js
@@ -1,6 +1,15 @@
 const initClickGreeting = () => {
+  let button = document.querySelector("#my-button");
+
+  button.addEventListener("click", () => {
+    // callback every time the button is clicked
     console.log("Welcome!");
-}
+  });
+};
+
+document.addEventListener("copy", () => {
+  console.log("D0nt COpy my things!");
+});
 
 // Sample usage - do not modify
 initClickGreeting();
--- Practice/0207_Practice/DOM_inter/dom24/index.js
+++ Practice/0207_Practice/DOM_inter/dom24/index.js
@@ -1,1 +1,7 @@
 // TODO: when the user clicks on '#menu-sidebar' => toggle the class 'show' on '#app-sidebar'
+let sideBar = document.querySelector("#menu-sidebar");
+let appSideBar = document.querySelector("#app-sidebar");
+
+sideBar.addEventListener("click", () => {
+  appSideBar.classList.toggle("show");
+});
