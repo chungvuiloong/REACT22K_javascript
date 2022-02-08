882dd96a466e0b7bfff67a84d5ef246900605657
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-08 14:50:14 +0200

    30 done

--- Practice/0207_Practice/DOM_events/dom30/index.js
+++ Practice/0207_Practice/DOM_events/dom30/index.js
@@ -1,1 +1,11 @@
 // TODO: implement tabs
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (event) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    event.currentTarget.classList.toggle("active");
+  });
+});
