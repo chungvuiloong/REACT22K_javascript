f782e4311e3ef4e8f00fd674800d6aae1b9fc893
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-09 09:49:52 +0200

    update

--- Practice/0207_Practice/DOM_events/dom29/index.js
+++ Practice/0207_Practice/DOM_events/dom29/index.js
@@ -1,23 +1,23 @@
-// TODO: The user can select a currency by clicking on it (add the class `active`). However, only one currency can be chosen at a time.
+TODO: The user can select a currency by clicking on it (add the class `active`). However, only one currency can be chosen at a time.
 
-// const currencyButtons = document.querySelectorAll("button");
+const currencyButtons = document.querySelectorAll("button");
 
-// currencyButtons.forEach((currencyButton) => {
-//   currencyButton.addEventListener("click", (event) => {
-//     console.log(currencyButton.textContent);
+currencyButtons.forEach((currencyButton) => {
+  currencyButton.addEventListener("click", (event) => {
+    console.log(currencyButton.textContent);
 
-//     currencyButton.currentTarget.toggle("active");
-//   });
-// });
-
-// correct algorithm
-const buttons = document.querySelectorAll(".card");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    buttons.forEach((button) => {
-      button.classList.remove("active");
-    });
-    event.currentTarget.classList.toggle("active");
+    debugger;
   });
 });
+
+// correct algorithm
+// const buttons = document.querySelectorAll(".card");
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     buttons.forEach((button) => {
+//       button.classList.remove("active");
+//     });
+//     event.currentTarget.classList.toggle("active");
+//   });
+// });
