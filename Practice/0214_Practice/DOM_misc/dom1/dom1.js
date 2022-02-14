65fbad1442cf64dabfdd115e4b5c8c09f48aa59f
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-14 12:06:21 +0200

    dom 1 done

--- Practice/0214_Practice/DOM_misc/dom1/dom1.js
+++ Practice/0214_Practice/DOM_misc/dom1/dom1.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
 Complete the renderShoppingList function such that it renders an <li> element
@@ -10,9 +10,12 @@ So, the first item should show up first (at the top).
 /**
  * @param {string[]} items
  */
-const renderShoppingList = items => {
-
-}
+const renderShoppingList = (items) => {
+  const list = document.querySelector("#shopping-list");
+  items.forEach((item) => {
+    list.insertAdjacentHTML("afterbegin", `<li>${item}</li>`); // DOnt use this, it can be hacked
+  });
+};
 // Sample usage - do not modify
 const sampleList = ["Orange", "Banana", "Coffee", "Paper"];
 console.log(renderShoppingList(sampleList));
