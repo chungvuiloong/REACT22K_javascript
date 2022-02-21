40c36c5d702e3bc6b39a2db2ae20dae81e165cec
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-21 09:51:29 +0200

    added new files

---
+++ 05_week/practice/script.js
@@ -0,0 +1,36 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header-container").classList.add("");
+  } else {
+    document.getElementById("header-container").classList.add("");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+///
+var mybutton = document.querySelector(".topCat");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
