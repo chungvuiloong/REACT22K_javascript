2af20975cc0e8e2caebf7ad4f7987fbf085406ab
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-23 10:41:13 +0200

    0223 commit

---
+++ testing/script.js
@@ -0,0 +1,23 @@
+// Animation for hero banner
+
+let darkSky = document.querySelector("#darkSky");
+let stars = document.querySelector("#stars");
+let moon = document.querySelector("#whiteMoon");
+let meteor = document.querySelector("#meteor");
+
+// let stars = document.querySelector("#stars");
+// let stars = document.querySelector("#stars");
+// let stars = document.querySelector("#stars");
+// let stars = document.querySelector("#stars");
+
+window.addEventListener("scroll", () => {
+  let yPosition = window.scrollY;
+
+  stars.style.left = yPosition * 0.25 + "px";
+  stars.style.top = yPosition * 0.25 + "px";
+
+  meteor.style.left = yPosition * 1 + "px";
+  meteor.style.top = yPosition * 1 + "px";
+
+  moon.style.marginTop = yPosition * 1.25 + "px";
+});
