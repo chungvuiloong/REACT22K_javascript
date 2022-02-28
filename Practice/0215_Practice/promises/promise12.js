b7260a22e1536c1ce40b31c8dfe6a4c635291268
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-28 12:43:08 +0200

    0228

--- Practice/0215_Practice/promises/promise12.js
+++ Practice/0215_Practice/promises/promise12.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
 We're using the same fakeFetch function from the last excercise that takes a string as a parameter. This fake fetch only handles the "flight-status" for now.
@@ -8,13 +8,13 @@ To handle the rejected case, you need to log the error using console.error.
 In the future, once we implement user-details, the code will automatically work because the promise will not reject.......*/
 
 const waitOneSecond = () => {
-    return new Promise(resolve => {
-        setTimeout(() => {
-        }, 1000);
-    });
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {}, 1000);
+    reject("Rejected");
+  });
+};
 
 // Sample usage - do not modify
 waitOneSecond().then(() => {
-    console.log("Done waiting.");
+  console.error("Done waiting.");
 });
