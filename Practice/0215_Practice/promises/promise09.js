d0757eacb8aaf7d2b55ed5f0adfa729c603ad09b
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-28 11:54:44 +0200

    0228

--- Practice/0215_Practice/promises/promise09.js
+++ Practice/0215_Practice/promises/promise09.js
@@ -29,9 +29,11 @@ To handle the rejected case, you need to log the error using console.error.
 In the future, once we implement user-details, the code will automatically work because the promise will not reject.......*/
 
 const logUserDetails = () => {
-  fakeFetch("flight-status").then((flightStatus) => {
-    console.log(flightStatus);
-  });
+  fakeFetch("user-details")
+    .then((e) => {
+      console.log(e);
+    })
+    .catch((error) => console.error(error));
 };
 
 // Sample usage (do not modify)
