7061fc9ef3fbeb61d2f0ac561e0ff1fbce98f312
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-03 14:00:53 +0200

    commit

--- 06_week/speedgame/main.js
+++ 06_week/speedgame/main.js
@@ -1,15 +1,93 @@
 let score = 0;
 
-let container = document.querySelector(".circleContainer");
-let circleButton = container.querySelector(".class");
-console.log(container.children[0]);
+firstButton = document.querySelector("#circle1");
+secondButton = document.querySelector("#circle2");
+thirdButton = document.querySelector("#circle3");
+startButton = document.querySelector(".start");
+endButton = document.querySelector(".end");
+overlay = document.querySelector(".overlay");
+
+// let container = document.querySelector(".circleContainer");
+// console.log(container);
 
 //WORKING Shows default 0 score
 document.querySelector("score").textContent = score;
 
-//WORKING Number of elements (3)
-let numCircle = document.querySelector(".circleContainer").childElementCount;
+const circles = document.querySelectorAll(".circle");
 
-for (let i = 1; i < numCircle; i++) {
-  console.log(container.children[i]);
+let active = 0;
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+circles.forEach((circle, i) =>
+  circle.addEventListener("click", () => clickedCircle(i))
+);
+
+const clickedCircle = (i) => {
+  i += 1;
+  console.log("clicked circle was:", i);
+};
+
+const startGame = () => {
+  console.log("Game Started");
+
+  let nextActive = pickNew(active);
+
+  circles[nextActive].classList.toggle("active");
+  circles[active].classList.remove("active");
+
+  active = nextActive;
+
+  console.log("active circle:", active);
+
+  timer = setTimeout(startGame, 1000);
+
+  function pickNew(active) {
+    let nextActive = getRndInteger(0, 3);
+
+    if (nextActive != active) {
+      return nextActive;
+    } else {
+      return pickNew(active);
+    }
+  }
+
+  //   function correct () {
+
+  //   }
+};
+
+const endGame = () => {
+  console.log("Game ended");
+  overlay.style.visibility = "visible";
+};
+
+const reloadGame = () => {
+  window.location.reload();
+};
+
+startButton.addEventListener("click", startGame);
+endButton.addEventListener("click", endGame);
+
+////////////////////////////////////////////////////////////////
+
+//WORKING Number of elements (3)
+// let numCircle = document.querySelector(".circleContainer").childElementCount;
+
+// for (let i = 0; i < numCircle; i++) {
+//   console.log(container.children[i]);
+// }
+
+// firstButton.addEventListener("click", () => {
+//   console.log("1 pressed");
+// });
+
+// secondButton.addEventListener("click", () => {
+//   console.log("2 pressed");
+// });
+
+// thirdButton.addEventListener("click", () => {
+//   console.log("3 pressed");
+// });
