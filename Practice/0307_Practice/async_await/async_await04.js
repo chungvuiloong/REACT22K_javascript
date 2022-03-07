6f8fac112f00284792fb99f02bf19490defcb539
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-07 13:50:49 +0200

    commit

--- Practice/0307_Practice/async_await/async_await04.js
+++ Practice/0307_Practice/async_await/async_await04.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
 /*
 Make the canVote instance method async. The sample usage code should then work as expected.
 */
 
 class User {
-    constructor(age) {
-        this.age = age;
-    }
+  constructor(age) {
+    this.age = age;
+  }
 
-    canVote() {
-        return this.age >= 18;
-    }
+  canVote = async (age) => {
+    return this.age >= 18;
+  };
 }
 
 // Sample usage - do not modify
 const user = new User(20);
-user.canVote().then(result => {
-    console.log(result); // true
+user.canVote().then((result) => {
+  console.log(result); // true
 });
