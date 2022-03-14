e2b0272021931a002865a8cd2ab298675970eef8
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-14 13:10:24 +0200

    commit

--- Practice/0314_Practice/todoapp/script.js
+++ Practice/0314_Practice/todoapp/script.js
@@ -5,17 +5,26 @@ const input     = document.getElementById("item");
 
 let itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
 
+//saving it locally
+localStorage.setItem("items", JSON.stringify(itemsArray));
+
+//getting it from local storage
+const data = JSON.parse(localStorage.getItem("items"));
+
 const listMaker = (text) => {
     const li = document.createElement("li");
     li.textContent = text;
     ul.appendChild(li);
 }
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-
-
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    itemsArray.push(input.value);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
     listMaker(input.value);
     input.value = "";
+});
+
+data.forEach((item) => {
+    listMaker(item);
 });
