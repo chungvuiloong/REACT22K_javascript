e42baedd98b05db42151da2ab08e06868d436e36
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-14 12:54:51 +0200

    commit

--- Practice/0314_Practice/todoapp/script.js
+++ Practice/0314_Practice/todoapp/script.js
@@ -3,6 +3,8 @@ const ul        = document.querySelector("ul");
 const button    = document.querySelector("button");
 const input     = document.getElementById("item");
 
+let itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
+
 const listMaker = (text) => {
     const li = document.createElement("li");
     li.textContent = text;
