58f6e9153549eb3c3a35cb7a1b23035dc2419a23
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-15 14:30:02 +0200

    commit

--- Practice/display-conversion-rates/index.js
+++ Practice/display-conversion-rates/index.js
@@ -1,15 +1,12 @@
 
 let baseCurrency = document.querySelector("#base-currency"); // The value is always 1
-let results = document.querySelector(".result");
-
 let targetCurrency = document.querySelector("#target-currency"); // Always changing
-let conversionResults = document.querySelector("#conversion-result"); // Always changing
 
 let input = document.querySelector("input");
-let secondConversion = document.querySelector("#conversion-results");
+let conversionResults = document.querySelector("#conversion-result"); // Always changing
+let secondConversion = document.querySelector("#conversion-results"); // Converts based on inputs
 // baseCurrency.selectedIndex
 
-// ${baseCurrency.value}
 function getConversionRates () {
 
   fetch("https://v6.exchangerate-api.com/v6/27f485a6f4826176bdcb3490/latest/" + baseCurrency.value) //USD
