216d32a093eb6238f4b40cdd3ad0333c879420ee
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-21 07:46:09 +0200

    commit

--- Practice/0307_Practice/async_await/async_await07/async_await07.js
+++ Practice/0307_Practice/async_await/async_await07/async_await07.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*
 Complete the getTweetDetails function such that it fetches the following endpoint
@@ -7,11 +7,16 @@ and then calls the showAuthorName function with the full name of tweet author
 (the full name should contain a space character  between the first name and the last name).
 You should use async/await */
 
-let URL =
-  "https://programmingjs-90a13-default-rtdb.europe-west1.firebasedatabase.app/tweet.json";
-
 const getTweetDetails = async () => {
   // TODO using async/await
+  const response = await fetch(
+    'https://programmingjs-90a13-default-rtdb.europe-west1.firebasedatabase.app/tweet.json'
+  );
+  const data = await response.json();
+  console.log(data);
+  showAuthorName(
+    data.author.details.firstName + ' ' + data.author.details.lastName
+  );
 };
 
 // do NOT modify this function
